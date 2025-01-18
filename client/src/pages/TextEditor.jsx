827d071f780e813c050106fbc1a9e05e32519f13
Ada <adaselcuk16@gmail.com>
2025-01-18 15:27:04 -0500

    minor changes - also send selected to same server

--- client/src/pages/TextEditor.jsx
+++ client/src/pages/TextEditor.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react';
 import './TextEditor.css'
 import rawData from '../../server/data.json'
 import { motion } from "motion/react"
-import { saveAs } from "file-saver";
 import axios from 'axios';
 
 const TextEditor = () => {
@@ -19,6 +18,7 @@ const TextEditor = () => {
 
         if (selectedText){
             setSelectedText(selectedText);
+            saveContentToServer(selectedText);
             console.log(selectedText);
         }
     }
